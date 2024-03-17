f989652aeaaa34654b326b5ad54b0c53c9fa6681
Devyadav0512 <devyadav5582@gmail.com>
2024-03-18 00:49:48 +0530

    [ADD]: Form UI

--- src/components/common/Form.js
+++ src/components/common/Form.js
@@ -1,33 +1,64 @@
 // @ts-nocheck
 import styles from "./common.module.css";
 
-function Form({ type, hideForm }) {
+function Form({ type, edit = false, hideForm }) {
   return (
     <div className={styles.formMainDiv}>
-      <h1>Add new {type}</h1>
-      <input placeholder="Name"></input>
-      <input placeholder="Your name"></input>
-      <input placeholder="Release Date" type="date"></input>
-      <input placeholder="Rating out of 10" type="number"></input>
-      <input placeholder="Review comments"></input>
-      <select
-        // onChange={(e) => {
-        //   changeScreen(e);
-        // }}
-        // className={styles.divDropDown}
-        name="movie"
-        id="movie"
-      >
-        <option value="movie">Movies</option>
-        <option value="review">Reviews</option>
-      </select>
-      <h1
-        onClick={() => {
-          hideForm();
-        }}
-      >
-        X
-      </h1>
+      <div className={styles.formDiv}>
+        <h1 className={styles.formHeader}>Add new {type}</h1>
+        {type == "movie" && (
+          <>
+            <input className={styles.formInput} placeholder="Name"></input>
+            <input
+              className={styles.formInput}
+              placeholder="Release Date"
+              onFocus={(e) => (e.target.type = "date")}
+              onBlur={(e) => (e.target.type = "text")}
+            ></input>
+          </>
+        )}
+
+        {type == "review" && (
+          <>
+            <select
+              // onChange={(e) => {
+              //   changeScreen(e);
+              // }}
+              name="movie"
+              id="movie"
+              className={styles.formInput}
+            >
+              <option value="movie">Movies</option>
+              <option value="review">Reviews</option>
+            </select>
+            <input className={styles.formInput} placeholder="Your name"></input>
+            <input
+              className={styles.formInput}
+              placeholder="Rating out of 10"
+              type="number"
+            ></input>
+            <textarea
+              className={styles.formInput}
+              placeholder="Review comments"
+            ></textarea>
+          </>
+        )}
+        <div className={styles.formButtonGroup}>
+          <button
+            onClick={() => {
+              hideForm();
+            }}
+            className={styles.formButton2}
+          >
+            Cancel
+          </button>
+          {type == "movie" ? (
+            <button className={styles.formButton1}>Add Movie</button>
+          ) : (
+            <button className={styles.formButton1}>Add Review</button>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
